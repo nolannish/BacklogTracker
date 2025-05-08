1827c6fe89d75512d64e0fb08ebe57b3888923f0
nolannish <nolannish@gmail.com>
2025-05-07 18:22:07 -0700

    start of database connections, including all new stuff for supabase

--- backlogtracker/src/app/register/page.tsx
+++ backlogtracker/src/app/register/page.tsx
@@ -11,71 +11,71 @@ export default function Register() {
       <div className="fixed inset-0 bg-black bg-opacity-40 flex items-center justify-center z-50">
         <div className="bg-white p-6 rounded-2xl shadow-lg w-full max-w-sm relative">
           <h2 className="text-2xl font-bold mb-4 text-center">Register</h2>
-					<form className="space-y-4">
-						<div>
-							<label htmlFor="firstName" className="block text-sm font-medium text-gray-700">
-								First Name
-							</label>
-							<input
-								type="text"
-								id="firstName"
-								name="firstName"
-								className="mt-1 w-full border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:ring-vlue-500"
-								placeholder="First Name"
-								required
-							/>
-							<label htmlFor="lastName" className="block text-sm font-medium text-gray-700">
-								Last Name
-							</label>
-							<input
-								type="text"
-								id="lastName"
-								name="lastName"
-								className="mt-1 w-full border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:ring-vlue-500"
-								placeholder="Last Name"
-								required
-							/> 
-							<label htmlFor="email" className="block text-sm font-medium text-gray-700">
-								Email
-							</label>
-							<input
-								type="email"
-								id="email"
-								name="email"
-								className="mt-1 w-full border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:ring-vlue-500"
-								placeholder="you@example.com"
-								required
-							/>
-							<label htmlFor="password" className="block text-sm font-medium text-gray-700">
-								Password
-							</label>
-							<input
-								type="password"
-								id="password"
-								name="password"
-								className="mt-1 w-full border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:ring-vlue-500"
-								placeholder="Password"
-								required
-							/>
-							<label htmlFor="confirmPassword" className="block text-sm font-medium text-gray-700">
-								Confirm Password
-							</label>
-							<input
-								type="password"
-								id="confirmPassword"
-								name="confirmPassword"
-								className="mt-1 w-full border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:ring-vlue-500"
-								placeholder="Confirm Password"
-								required
-							/>
-							<button
-								type="submit"
-								className="w-full bg-blue-600 text-white py-2 px-4 mt-4 rounded-md hover:bg-blue-700 active:scale-95 transition"
-							>
-								Create Account
-							</button>
-						</div>
-					</form>
+			<form className="space-y-4">
+				<div>
+					<label htmlFor="firstName" className="block text-sm font-medium text-gray-700">
+						First Name
+					</label>
+					<input
+						type="text"
+						id="firstName"
+						name="firstName"
+						className="mt-1 w-full border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:ring-vlue-500"
+						placeholder="First Name"
+						required
+					/>
+					<label htmlFor="lastName" className="block text-sm font-medium text-gray-700">
+						Last Name
+					</label>
+					<input
+						type="text"
+						id="lastName"
+						name="lastName"
+						className="mt-1 w-full border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:ring-vlue-500"
+						placeholder="Last Name"
+						required
+					/> 
+					<label htmlFor="email" className="block text-sm font-medium text-gray-700">
+						Email
+					</label>
+					<input
+						type="email"
+						id="email"
+						name="email"
+						className="mt-1 w-full border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:ring-vlue-500"
+						placeholder="you@example.com"
+						required
+					/>
+					<label htmlFor="password" className="block text-sm font-medium text-gray-700">
+						Password
+					</label>
+					<input
+						type="password"
+						id="password"
+						name="password"
+						className="mt-1 w-full border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:ring-vlue-500"
+						placeholder="Password"
+						required
+					/>
+					<label htmlFor="confirmPassword" className="block text-sm font-medium text-gray-700">
+						Confirm Password
+					</label>
+					<input
+						type="password"
+						id="confirmPassword"
+						name="confirmPassword"
+						className="mt-1 w-full border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:ring-vlue-500"
+						placeholder="Confirm Password"
+						required
+					/>
+					<button
+						type="submit"
+						className="w-full bg-blue-600 text-white py-2 px-4 mt-4 rounded-md hover:bg-blue-700 active:scale-95 transition"
+					>
+						Create Account
+					</button>
+				</div>
+			</form>
         </div>
       </div>
     </main>
