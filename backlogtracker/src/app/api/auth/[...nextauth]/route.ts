6aeedd12c4dac099907b7d666b1e609e75f89345
nolannish <nolannish@gmail.com>
2025-06-03 22:25:21 -0700

    temp change to see if this deploys

--- backlogtracker/src/app/api/auth/[...nextauth]/route.ts
+++ backlogtracker/src/app/api/auth/[...nextauth]/route.ts
@@ -1,12 +1,6 @@
 import NextAuth from 'next-auth';
 import Steam from 'next-auth-steam';
 import type { NextRequest } from 'next/server';
-import { SteamSignIn } from '../../database/SteamSignIn';
-
-interface SteamProfile { 
-  steamid: string;
-  personaname: string;
-}
 
 async function auth(
   req: NextRequest,
@@ -21,29 +15,7 @@ async function auth(
       Steam(req, {
         clientSecret: process.env.STEAM_API_KEY!
       })
-    ],
-     callbacks: {
-      async signIn({ profile }) {
-        if (!profile || !('steamid' in profile) || !('personaname' in profile)) {
-          console.error('Missing or invalid Steam profile');
-          return false;
-        }
-
-        const steamId = (profile as SteamProfile).steamid;
-        const username = (profile as SteamProfile).personaname;
-
-        const result = await SteamSignIn(steamId, username);
-        return result.success;
-      },
-
-      async session({ session, token }) {
-        if (session.user) {
-          session.user.id = token.sub;
-        }
-        return session;
-      },
-    },
-    secret: process.env.NEXTAUTH_SECRET,
+    ]
   })
 }
 
